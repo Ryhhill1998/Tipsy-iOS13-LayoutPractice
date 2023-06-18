2cbd61bbf8761b5c85dcb04b2ef3c7c18e7b2fad
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-18 22:33:59 +0100

    Started adding basic actions to calculator

--- Tipsy/Controllers/ViewController.swift
+++ Tipsy/Controllers/ViewController.swift
@@ -9,26 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tip: Float?
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var splitLabel: UILabel!
     
+    @IBOutlet weak var tip0Button: UIButton!
+    @IBOutlet weak var tip10Button: UIButton!
+    @IBOutlet weak var tip20Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-    @IBAction func tipPressed(_ sender: UIButton) {
+        resetTipButtons()
     }
     
+    func resetTipButtons() {
+        tip0Button.isSelected = false
+        tip10Button.isSelected = false
+        tip20Button.isSelected = false
+    }
+    
+    @IBAction func tipPressed(_ sender: UIButton) {
+        resetTipButtons()
+        tip = (Float(sender.titleLabel!.text!.components(separatedBy: "%")[0]) ?? 0.0) / 100.0
+        sender.isSelected = true
+    }
     
     @IBAction func splitChanged(_ sender: UIStepper) {
         splitLabel.text = "\(Int(sender.value))"
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        let totalBill = (Float(billTextField.text! ) ?? 0.0) * (1 + (tip ?? 0.0))
+        print(totalBill)
     }
 }
 
